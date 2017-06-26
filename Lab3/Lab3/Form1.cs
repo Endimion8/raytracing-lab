227d3305cadfc3667c48482137162d05c0fb4f04
Endemion8 <steam.sparrow8@gmail.com>
2017-06-26 13:56:04 +0300

    Рабочий вариант трассировки лучей на форме.

---
+++ Lab3/Lab3/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            glControl1.Invalidate();
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+
+            GL.Begin(BeginMode.Quads);
+            GL.TexCoord2(0.0, 0.0); GL.Vertex3(-1.0, -1.0, 1);
+            GL.TexCoord2(1.0, 0.0); GL.Vertex3(1.0, -1.0, 1);
+            GL.TexCoord2(1.0, 1.0); GL.Vertex3(1.0, 1.0, 1);
+            GL.TexCoord2(0.0, 1.0); GL.Vertex3(-1.0, 1.0, 1);
+            GL.End();
+
+            View m = new View();
+
+            Console.WriteLine(m.glslVersion);
+            Console.WriteLine(m.glVersion);
+
+            m.InitShaders(/*glControl1.Width / (float)glControl1.Height*/);
+
+            GL.Viewport(0, 0, glControl1.Width, glControl1.Height);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.Enable(EnableCap.DepthTest);
+            GL.EnableVertexAttribArray(m.attribute_vpos);
+            Console.WriteLine("OK");
+            GL.DrawArrays(PrimitiveType.Quads, 0, 4);
+            GL.DisableVertexAttribArray(m.attribute_vpos);
+
+            glControl1.SwapBuffers();
+            GL.UseProgram(0);
+        }
+    }
+}
---
+++ Lab3/Lab3/View.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Lab3
+{
+    class View
+    {
+        int BasicProgramID;
+        int BasicVertexShader;
+        int BasicFragmentShader;
+
+        public string glVersion = GL.GetString(StringName.Version);
+        public string glslVersion = GL.GetString(StringName.ShadingLanguageVersion);
+
+        int vbo_position;
+        Vector3[] vertdata;
+        public int attribute_vpos;
+        public int uniform_pos;
+        public int uniform_aspect;
+        public double aspect;
+        public int uniform_mview;
+        public Vector3 campos = new Vector3(1.0f, 0, 0);
+
+        public void loadShader(String filename, ShaderType type, int program, out int address)
+        {
+            address = GL.CreateShader(type);
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(filename))
+            {
+                GL.ShaderSource(address, sr.ReadToEnd());
+            }
+            GL.CompileShader(address);
+            GL.AttachShader(program, address);
+            Console.WriteLine(GL.GetShaderInfoLog(address));
+        }
+
+        public void InitShaders(/*float character_size*/)
+        {
+            vertdata = new Vector3[] {
+                new Vector3(-1f, -1f, 0f),
+                new Vector3( 1f, -1f, 0f),
+                new Vector3( 1f, 1f, 0f),
+                new Vector3(-1f, 1f, 0f)
+            };
+            BasicProgramID = GL.CreateProgram();  // создание объекта программы  
+            loadShader("..\\..\\raytracing.vert", ShaderType.VertexShader, BasicProgramID, out BasicVertexShader);
+            loadShader("..\\..\\raytracing.frag", ShaderType.FragmentShader, BasicProgramID, out BasicFragmentShader);
+            GL.LinkProgram(BasicProgramID); // Проверяем успех компоновки 
+            int status = 0;
+            GL.GetProgram(BasicProgramID, GetProgramParameterName.LinkStatus, out status);
+            Console.WriteLine(GL.GetProgramInfoLog(BasicProgramID));
+
+            GL.GenBuffers(1, out vbo_position);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo_position);
+            GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, (IntPtr)(vertdata.Length *
+            Vector3.SizeInBytes), vertdata, BufferUsageHint.StaticDraw);
+            GL.VertexAttribPointer(attribute_vpos, 3, VertexAttribPointerType.Float, false, 0, 0);
+            GL.Uniform3(uniform_pos, campos);
+            GL.Uniform1(uniform_aspect, aspect);
+            GL.UseProgram(BasicProgramID);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+
+
+        }
+    }
+}
